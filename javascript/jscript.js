fab90ae06d6b9580cf1d73040718ab416484f086
Shuma Gorath <jmgordon1988@gmail.com>
2018-11-29 00:08:12 -0800

    stylin and profilin'

--- javascript/jscript.js
+++ javascript/jscript.js
@@ -4,7 +4,7 @@ function makeButtons() {
 
     $("#buttonList").empty();
 
-    for (var i = 0; i < topics.length; i++){
+    for (var i = 0; i < topics.length; i++) {
         var a = $("<button style='padding: 5px; margin: 5px;'>");
         a.addClass("character");
         a.attr("data-name", topics[i]);
@@ -13,7 +13,7 @@ function makeButtons() {
     }
 }
 
-$("#add-character").on("click", function(event){
+$("#add-character").on("click", function (event) {
     event.preventDefault();
 
     var character = $("#character-input").val().trim();
@@ -26,38 +26,38 @@ $("#add-character").on("click", function(event){
 
 makeButtons();
 
-$("button").on("click", function(){
+$("button").on("click", function () {
     $("#gifshere").empty();
     var slasher = $(this).attr("data-name");
 
     var key = "OhZvd5m3Bz8gbjnHIf8IBQOvBI9szvQy";
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+slasher+"&api_key="+key+"&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + slasher + "&api_key=" + key + "&limit=10";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-    .then(function(response){
-        console.log(queryURL);
-        console.log(response);
-
-        var results = response.data;
-
-        for (var i = 0; i < results.length; i++) {
-            var characterDiv = $("<div>");
-
-            var p = $("<p>").text("Rating: "+results[i].rating);
-
-            var charImage = $("<img style='padding: 5px;'>");
-            charImage.attr("src", results[i].images.fixed_height.url);
-            charImage.attr("width", "250");
-            charImage.attr("height", "125");
-
-            characterDiv.append(p);
-            characterDiv.append(charImage);
-
-            $("#gifshere").prepend(characterDiv);
-        }
+    $.ajax({
+        url: queryURL,
+        method: "GET"
     })
+        .then(function (response) {
+            console.log(queryURL);
+            console.log(response);
+
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var characterDiv = $("<div>");
+
+                var p = $("<p>").text("Rating: " + results[i].rating);
+
+                var charImage = $("<img style='padding: 5px;'>");
+                charImage.attr("src", results[i].images.fixed_height.url);
+                charImage.attr("width", "250");
+                charImage.attr("height", "125");
+
+                characterDiv.append(p);
+                characterDiv.append(charImage);
+
+                $("#gifshere").prepend(characterDiv);
+            }
+        })
 })
